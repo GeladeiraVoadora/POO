eee3dfcc7f5c426daf48525468dfa1446a60107b
Luís Eduardo <102266309+GeladeiraVoadora@users.noreply.github.com>
2024-10-10 16:11:34 -0300

    Usuário controller

--- eventhub/src/main/java/br/org/upe/eventhub/controllers/EventoController.java
+++ eventhub/src/main/java/br/org/upe/eventhub/controllers/EventoController.java
@@ -6,11 +6,9 @@ import br.org.upe.eventhub.services.EventoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/eventos")
@@ -35,7 +33,7 @@ public class EventoController {
         return ResponseEntity.status(404).body(null);
     }
 
-    @PostMapping()
+    @PostMapping
     public ResponseEntity<Evento> criarEvento(@RequestBody Evento evento){
         try {
             Evento novoEvento = eventoService.cadastrarEvento(evento);
--- eventhub/src/main/java/br/org/upe/eventhub/services/UsuarioService.java
+++ eventhub/src/main/java/br/org/upe/eventhub/services/UsuarioService.java
@@ -1,13 +1,14 @@
 package br.org.upe.eventhub.services;
 
 import br.org.upe.eventhub.entities.Usuario;
+import br.org.upe.eventhub.exceptions.UsuarioExistenteException;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UsuarioService {
 
-    public Usuario cadastraUsuario(Usuario usuario) throws Exception;
+    public Usuario cadastraUsuario(Usuario usuario) throws UsuarioExistenteException;
     public Usuario atualizarUsuario(Usuario usuario);
     public void removerUsuario(Usuario usuario);
     public List<Usuario> listarUsuarios();
